d14474f50ef5d20513eadadbb7bb97bd47fd2759
Nav9n <naveen65.ny@gmail.com>
2023-08-24 18:35:32 +0000

    push

--- src/main/java/com/simplilearn/petadoption/controllers/ApplicationController.java
+++ src/main/java/com/simplilearn/petadoption/controllers/ApplicationController.java
@@ -3,7 +3,7 @@ package com.simplilearn.petadoption.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class ApplicationController {
     @RequestMapping("/")
     public String startApplication(){
